f59e6c3603732004620106ed85d092592b5a026f
jdzapata931224 <94489853+jdzapata931224@users.noreply.github.com>
2026-03-07 08:04:59 -0500

    repositorio

--- src/domain/repositories/IUsuariosRepository.js
+++ src/domain/repositories/IUsuariosRepository.js
@@ -3,6 +3,7 @@ const { NotImplementedError } = require('../exceptions/UsuariosErrors');
 class IUsuariosRepository {
   
   async findById(id) { throw new NotImplementedError(); }
+  async find(filter) { throw new NotImplementedError(); }
 }
 
 module.exports = IUsuariosRepository;
