ac89bceca16c4cda7478685b2d5ce0ff63b91d9e
jdzapata931224 <94489853+jdzapata931224@users.noreply.github.com>
2026-03-07 07:56:48 -0500

    cabio

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.use((req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//connectDB();
+connectDB();
 
 module.exports = app;
