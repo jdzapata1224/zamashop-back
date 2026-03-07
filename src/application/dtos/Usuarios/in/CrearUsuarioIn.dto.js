607e2d5f125d153b593532426f192d24e368f5dc
jdzapata931224 <94489853+jdzapata931224@users.noreply.github.com>
2026-03-07 16:56:28 -0500

    cambios

--- src/application/dtos/Usuarios/in/CrearUsuarioIn.dto.js
+++ src/application/dtos/Usuarios/in/CrearUsuarioIn.dto.js
@@ -1,16 +1,16 @@
 class CrearUsuarioInDTO {
   constructor({ primer_nombre,segundo_nombre, primer_apellido,segundo_apellido, usuario, password, identificacion, correo, telefono, usuarioCreacion }) {
     if (!primer_nombre        || typeof primer_nombre        !== 'string' || !primer_nombre.trim())        throw new Error('Primer Nombre es requerido');
-    if (typeof segundo_nombre        !== 'string' || !segundo_nombre.trim())        throw new Error('Segundo Nombre es requerido');
-
+    
     if (!primer_apellido      || typeof primer_apellido      !== 'string' || !primer_apellido.trim())      throw new Error('apellidos es requerido');
-    if (typeof segundo_apellido      !== 'string' || !segundo_apellido.trim())      throw new Error('Segundo Apellido es requerido');
-
     if (!usuario        || typeof usuario        !== 'string' || !usuario.trim())        throw new Error('usuario es requerido');
     if (!password       || typeof password       !== 'string' || !password.trim())       throw new Error('password es requerido');
     if (!identificacion || typeof identificacion !== 'string' || !identificacion.trim()) throw new Error('identificacion es requerida');
     if (!correo         || typeof correo         !== 'string' || !correo.trim())         throw new Error('correo es requerido');
     if (!telefono       || typeof telefono       !== 'string' || !telefono.trim())       throw new Error('telefono es requerido');
+    
+     if (segundo_nombre  !== undefined && segundo_nombre  !== null && typeof segundo_nombre  !== 'string') throw new Error('Segundo nombre debe ser texto');
+    if (segundo_apellido !== undefined && segundo_apellido !== null && typeof segundo_apellido !== 'string') throw new Error('Segundo apellido debe ser texto');
 
     this.primer_nombre      = primer_nombre.trim();
     this.segundo_nombre   = segundo_nombre?.trim()   || null;
