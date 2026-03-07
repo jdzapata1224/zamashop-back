8429a1cb3b797d3d485c7ef8250ecc721e4a56b8
jdzapata931224 <94489853+jdzapata931224@users.noreply.github.com>
2026-03-06 23:59:39 -0500

    test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Reemplaza con TU string de conexión
-const uri = 'mongodb+srv://zamashop-user:Ap69WONrxbnGUrUI@clusterdev.bn4cwwc.mongodb.net/?appName=ClusterDev';
+const uri = 'mongodb+srv://zamashop-user:d2OMguAda7gG7wen@clusterdev.bn4cwwc.mongodb.net/?appName=ClusterDev';
 console.log('Intentando conectar a:', uri.substring(0, 30) + '...');
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
