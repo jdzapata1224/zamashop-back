ad165733b7076906ade26573cd55c9fd413bb209
jdzapata931224 <94489853+jdzapata931224@users.noreply.github.com>
2026-03-07 13:45:33 -0500

    cambio

--- src/interfaces/http/controllers/CrearUsuarioController.js
+++ src/interfaces/http/controllers/CrearUsuarioController.js
@@ -8,6 +8,10 @@ class CrearUsuarioController {
 
   crearUsuario  = async (req, res) => {
     try {
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(200).json({ codigo: 400, message: 'No Se Ha Recibido Ningun Parametro' });
+    }
       const output = await this.crearUsuarioUseCase.execute(req.body);  
 
       return res.status(200).json({
