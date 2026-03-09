4690edd9880ea74d537c0122ac309f948a805a90
jdzapata931224 <94489853+jdzapata931224@users.noreply.github.com>
2026-03-09 12:13:39 -0500

    auth

--- src/interfaces/http/routes/AuthRoutes.js
+++ src/interfaces/http/routes/AuthRoutes.js
@@ -3,9 +3,9 @@ const router   = express.Router();
 
 const UsuariosSchemaRepository = require('../../../infrastructure/database/repositories/UsuariosSchemaRepository');
 const LoginUseCase             = require('../../../application/use-cases/Auth/Login');
-const LoginController          = require('../controllers/LoginController');
+const LoginController          = require('../controllers/Auth/LoginController');
 const ValidarTokenUseCase             = require('../../../application/use-cases/Auth/ValidarToken');
-const ValidarTokenController          = require('../controllers/ValidarTokenController');
+const ValidarTokenController          = require('../controllers/Auth/ValidarTokenController');
 
 
 const userRepository  = new UsuariosSchemaRepository();
